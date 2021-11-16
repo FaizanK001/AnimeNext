0c758681256f64b6543921810e3993615c1038f6
Farshad Shafii <farshad.shafii@gmail.com>
2021-11-16 21:43:52 +0000

    components/footer.js: add: updated the footer to have just menu and newsletter

--- components/footer.js
+++ components/footer.js
@@ -1,15 +1,10 @@
 const Footer = () => {
   return (
     <>
-      <div>
-        Menu
-      </div>
-      <div>
-        Newsletter
-      </div>
-      <div>
-        Copyright 2021 - Next Anime
-      </div>
+      <footer>
+        <div>Menu</div>
+        <div>Newsletter</div>
+      </footer>
     </>
   );
 };
